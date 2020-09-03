706410f087b85cd56941d9d22cfde54e48de82ac
Łukasz <lewak.lukasz@gmail.com>
2020-09-03 15:51:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './css/App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Results from "./components/Results";
+
+
+function App() {
+  return (
+    <div className="app">
+
+        <Header/>
+
+        <Nav />
+
+        <Results />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/VideoCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../css/VideoCard.css";
+
+function VideoCard({ movie }) {
+    return(
+        <div className="videoCard">
+            <h1>helo</h1>
+            <img src="" alt=""/>
+            <p>movie desc</p>
+            <h2>mov title</h2>
+            <p>num of likes</p>
+        </div>
+
+    )
+
+}
+
+export default VideoCard;
