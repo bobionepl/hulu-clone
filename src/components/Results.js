acb1245618a36c00845b2934575905739f8fdd4d
Łukasz <lewak.lukasz@gmail.com>
2020-09-08 11:40:21 +0200

    third commit

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,31 +3,32 @@ import "../css/Results.css";
 import VideoCard from './VideoCard'
 import {useEffect} from "react";
 import axios from "./axios"
-import requests from "../requests";
+import FlipMove from "react-flip-move";
+// import requests from "../requests";
 
 
-function Results(){
+function Results({ selectedOption }){
 
     const [movies, setMovies] = useState([])
 
     useEffect(()=>{
         async function fetchData() {
-               const request = await axios.get(requests.fetchActionMovies);
+               const request = await axios.get(selectedOption);
                setMovies(request.data.results);
             console.log(request)
                return request
         }
 
         fetchData();
-    },[])
+    },[selectedOption])
 
     return (
         <div className="results">
-
+            <FlipMove>
             {movies.map((movie)=>(
-                <VideoCard movie={movie} />
+                <VideoCard key={movie.id} movie={movie} />
             ))}
-
+            </FlipMove>
         </div>
 
     )
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, {forwardRef} from "react";
 import "../css/VideoCard.css";
 import Results from "./Results";
+import {ThumbUpSharp} from "@material-ui/icons"
+
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
-function VideoCard({ movie }) {
+const VideoCard = forwardRef(({ movie }, ref) => {
     return(
-        <div className="videoCard">
+        <div ref={ref} className="videoCard">
 
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="movie img"/>
             <p>{movie.overview}</p>
-            <h2>{movie.title}</h2>
-            <p>{movie.vote_count} {movie.relase_date}</p>
+            <h2>{movie.title || movie.original_name}</h2>
+            <p className='videoCard_stats'>
+                {movie.media_type && `${movie.media_type} •` }
+                {movie.release_date || movie.first_air_date} •
+                <ThumbUpSharp />{" "}
+            {movie.vote_count} </p>
         </div>
 
     )
 
-}
+});
 
 export default VideoCard;
